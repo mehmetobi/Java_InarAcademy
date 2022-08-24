24ae6ca85cf8062dfeeec2bc6ac6503794c2bb10
Mehmet obi <mehmetobi01@gmail.com>
2022-08-24 13:28:56 +0300

    Display time function written. Pow function example added.

--- src/chapters/src/Chapter_01/Exercises_01.java
+++ src/chapters/src/Chapter_01/Exercises_01.java
@@ -5,21 +5,6 @@ import java.util.Scanner;
 public class Exercises_01 {
 
 	  public static void main(String[] args) {
-		    Scanner veri_oku = new Scanner(System.in);  // Create a Scanner object
-		    System.out.println("Enter name");
-		    String name = veri_oku.next(); 
-		    System.out.println("Enter surname");
-		    String surname = veri_oku.next(); 
-		    System.out.println("Enter age:");
-		    int age = veri_oku.nextInt(); 
-		    System.out.println("what is your favorite team ? :");
-		    String team = veri_oku.next(); 
-		    
-		    
-		    System.out.println("Name: " + name); 
-		    System.out.println("Surname: " + surname);
-		    System.out.println("Age: " + age);  
-		    System.out.println("Team: " + team); 
-		    
+		
 	 }
 }
--- src/chapters/src/Chapter_02/Checkpoint_02.java
+++ src/chapters/src/Chapter_02/Checkpoint_02.java
@@ -1,5 +1,43 @@
 package Chapter_02;
 
+import java.util.Scanner;
+
 public class Checkpoint_02 {
 
+	 public static void main (String [] args ) {
+
+		 System.out.println(" Saniye cinsinden bir tam sayı giriniz: ");	 
+		   Scanner klavye = new Scanner(System.in);
+	
+		   int saniye = klavye.nextInt();
+		   int dakika = saniye / 60;
+		   int kalanSaniye = saniye % 60;
+		   
+		   System.out.println(saniye  + " saniye "  + dakika + " dakika ve " +  kalanSaniye +  " saniyedir " );
+	  
+		   //--------------------------------------------------------------
+		   
+		   double x = 2 ;
+		   double y = 3 ;
+		double islem = üs_alma (x, y ) ;
+ 
+		System.out.println(islem );
+		
+		 
+			 }
+	 
+	 
+	
+public static double üs_alma(double x , double y ) {
+	
+	
+	return Math.pow (x,y);
+	
+	
+	
+}
+	 
+	 
+	 
+	 
 }
