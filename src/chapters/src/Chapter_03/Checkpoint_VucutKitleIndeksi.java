02ee80ef8a2b55b7884ca9b6e8b37f8fe94e2ab3
Mehmet obi <mehmetobi01@gmail.com>
2022-09-02 23:31:14 +0300

    Chapter03 Exercises 06,07,08,09,11 added

--- src/chapters/src/Chapter_03/Checkpoint_VucutKitleIndeksi.java
+++ src/chapters/src/Chapter_03/Checkpoint_VucutKitleIndeksi.java
@@ -3,34 +3,30 @@ package Chapter_03;
 import java.util.Scanner;
 
 public class Checkpoint_VucutKitleIndeksi {
-	public static void main (String [] args ) {
-	
-	Scanner klavye = new Scanner(System.in) ;
-	
-	System.out.println("Agırlıgı kg cinsinden giriniz: ");
-	double agirlik = klavye.nextDouble() ;
-	
-	System.out.println("Boyunuzu metre cinsinden yazınız: ");	
-	double boy = klavye.nextDouble() ;
-	
-	double kitleindeksi = agirlik / ( boy * boy ) ;
-	
-	System.out.println(" Vücut kitle indeksi: " + kitleindeksi +"dir");	
-		
-		
-	if (kitleindeksi < 18.5) {
-		
-    System.out.println("Çok zayıf");		
-	}else if ( kitleindeksi < 25) {
+	public static void main(String[] args) {
+
+		Scanner klavye = new Scanner(System.in);
+
+		System.out.println("Agırlıgı kg cinsinden giriniz: ");
+		double agirlik = klavye.nextDouble();
+
+		System.out.println("Boyunuzu metre cinsinden yazınız: ");
+		double boy = klavye.nextDouble();
+
+		double kitleindeksi = agirlik / (boy * boy);
+
+		System.out.println(" Vücut kitle indeksi: " + kitleindeksi + "dir");
+
+		if (kitleindeksi < 18.5) {
+
+			System.out.println("Çok zayıf");
+		} else if (kitleindeksi < 25) {
 			System.out.println("Normal");
-	}else if ( kitleindeksi < 30) {
-		System.out.println("Kilolu");
-	}else {
-		System.out.println("Obez");
-	}
-	
-	
-	
-	
+		} else if (kitleindeksi < 30) {
+			System.out.println("Kilolu");
+		} else {
+			System.out.println("Obez");
+		}
+
 	}
 }
